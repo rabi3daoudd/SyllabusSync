5e895cf5cb7b61e8114a924e54c5411be1ef7dff
Jlabe <jlabe096@uottawa.ca>
2024-01-28 01:16:47 -0500

    fix

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// You can comment out getAnalytics if you're not using it currently
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +19,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Export the app variable for use in other parts of the application
+export { app };
+
+// Comment out or remove the analytics if not in use
+// const analytics = getAnalytics(app);
