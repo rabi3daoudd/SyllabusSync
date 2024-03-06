d8266b84f41c6b32067005dad8825efc3c590051
Rabih Daoud <71355928+rabi3daoudd@users.noreply.github.com>
2024-03-05 20:28:19 -0500

    Revert "Built navbar component"

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,11 @@ import { AuthProvider } from './components/AuthContext';
 import SignInButton from './components/SignInButton';
 import FetchUserCalendars from './components/FetchUserCalendars'
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import Navbar from './components/ui/Navbar';
+
 function App() {
   //const { SignInWithGoogleButton, isAuthenticated } = useCustomGoogleLogin();
 
   return (
-    <>
-    <Navbar/>
     <GoogleOAuthProvider clientId="PLACE CLIENT ID HERE">
       <AuthProvider>
         {/* <SignIn /> */}
@@ -23,7 +21,6 @@ function App() {
         <FetchUserCalendars />
       </AuthProvider>
     </GoogleOAuthProvider>
-    </>
 
   )
 }
