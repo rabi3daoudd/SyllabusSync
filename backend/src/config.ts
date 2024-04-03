7d11722e41620c4dd3abe9dc5e91edd2ac5611b2
Jlabe <jlabe096@uottawa.ca>
2024-04-03 01:15:03 -0400

    added env for vercel deployment

---
+++ backend/src/config.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+export const clientId = process.env.CLIENT_ID!;
+export const clientSecret = process.env.CLIENT_SECRET!;
+
+const serviceAccountString = process.env.FIREBASE_ADMINSDK!;
+const decodedServiceAccountString = decodeURIComponent(serviceAccountString);
+const serviceAccount = JSON.parse(decodedServiceAccountString);
+
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
+export const firebaseServiceAccount = serviceAccount;
