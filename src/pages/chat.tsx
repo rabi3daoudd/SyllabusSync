291ffbbcd6d6b5d02681687ace2df8eb1fbf0c72
Rabih Daoud <71355928+rabi3daoudd@users.noreply.github.com>
2024-04-04 21:16:09 -0400

    Update chat.tsx

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,8 +1,23 @@
-"use client";
-
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { auth } from "../firebase-config";
 import ChatBot from "../components/assistant/Chat";
 import Navbar from "../components/navigation/Navbar";
 import "../app/globals.css";
+
+const router = useRouter();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (!user) {
+        // User is not authenticated, redirect to the login page
+        router.push("/login");
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
 
 export default function Chat() {
   return (
