4d471dd1e6f592826d8cc67b5af0ac3e90e85697
Rabih Daoud <71355928+rabi3daoudd@users.noreply.github.com>
2024-04-06 22:04:54 -0400

    Navbar stops showing on /login /signup
    Co-Authored-By: AliKhreis <80130844+alikhreis7@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import Navbar from "@/components/navigation/Navbar";
+import { useRouter } from "next/router";
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const router = useRouter();
+  const shouldShowNavbar =
+    router.pathname !== "/login" && router.pathname !== "/signup";
+
   return (
     <>
-      <Navbar />
+      {shouldShowNavbar && <Navbar />}
       <main>{children}</main>
     </>
   );
