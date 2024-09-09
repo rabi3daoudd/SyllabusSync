181572b6794c46fc8dc371e4fc8a832a1dea600b
Jlabe <jlabe096@uottawa.ca>
2024-09-08 22:32:11 -0400

    deleted backend

--- backend/src/firebaseAdmin.ts
+++
@@ -1,8 +0,0 @@
-const admin = require('firebase-admin');
-import {firebaseServiceAccount} from "./config";
-
-admin.initializeApp({
-    credential: admin.credential.cert(firebaseServiceAccount),
-});
-
-module.exports = admin;
