aad7c40ec45e064c9bdea1f65c8e9776de9c1ef2
Jeyason Jeyaparan <56597078+jey12345678@users.noreply.github.com>
2024-10-21 22:42:22 -0400

    SYL-71 Created two unit tests for the classes page. (#63)
    * SYL-71 Created two unit tests for the classes page.
    
    * SYL-71 Created two unit tests for the classes page.
    
    * SYL-71 Created two unit tests for the classes page.
    
    * SYL-71 Created two unit tests for the classes page.
    
    * SYL-71 Created two unit tests for the classes page.
    
    * SYL-71 Created two unit tests for the classes page.
    
    * SYL-71 Created two unit tests for the classes page.

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,23 +1,53 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  setupFiles: ['<rootDir>/jest.setup.ts'],
-  transform: {
-    '^.+\\.(ts|tsx|js|jsx)?$': 'ts-jest',
-  },
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  testMatch: [
-    '**/apiTest/**/*.test.(ts|tsx|js|jsx)',
-    '**/?(*.)+(spec|test).[jt]s?(x)',
-  ],
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.jest.json',
-      isolatedModules: true,
+  projects: [
+    {
+      displayName: "react",
+      preset: 'ts-jest',
+      testEnvironment: 'jsdom',
+      transform: {
+        '^.+\\.(ts|tsx|js|jsx)?$': 'ts-jest',
+      },
+      moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+      testMatch: [
+        '**/tests/classes/**/*.test.(ts|tsx|js|jsx)',
+      ],
+      transformIgnorePatterns: [
+        "node_modules/(?!firebase|@firebase)"
+      ],
+      moduleNameMapper: {
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+        '^@/(.*)$': '<rootDir>/src/$1',
+      },
+      globals: {
+        'ts-jest': {
+          tsconfig: 'tsconfig.jest.json',
+          isolatedModules: true,
+        },
+      },
     },
-  },
-}; 
+    {
+      displayName: "api",
+      preset: 'ts-jest',
+      testEnvironment: 'node',
+      setupFiles: ['<rootDir>/jest.setup.ts'],
+      transform: {
+        '^.+\\.(ts|tsx|js|jsx)?$': 'ts-jest',
+      },
+      moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+      testMatch: [
+        '**/tests/apiTest/**/*.test.(ts|tsx|js|jsx)',
+        '**/tests/App.test.tsx'
+      ],
+      moduleNameMapper: {
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+        '^@/(.*)$': '<rootDir>/src/$1',
+      },
+      globals: {
+        'ts-jest': {
+          tsconfig: 'tsconfig.jest.json',
+          isolatedModules: true,
+        },
+      },
+    },
+  ],
+};
